31719998198ad3796f227b6a588aabe7399141cd
JessieDev <118378954+jesrqt@users.noreply.github.com>
2023-05-03 16:32:52 +0200

    Project swiper and hamburger menu completed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,32 +1,103 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 import './NavBar.css';
 
+const getIsHamburger = () => window.innerWidth <= 1000;
+
 export const NavBar = () => {
-    return (
-        <nav>
-            <div className="eighty-width nav-container">
-                <a href="https://jessieinit.com/" id="logo">{'>'}_Jessie_in_IT</a>
-                <ul>
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [isHamburgerMenu, setIsHamburgerMenu] = useState(getIsHamburger())
+
+    useEffect(() => {
+        const onResize = () => {
+            setIsHamburgerMenu(getIsHamburger())
+        };
+        window.addEventListener("resize", onResize);
+
+        return () => {
+            window.removeEventListener("resize", onResize)
+        }
+    }, []);
+
+    const toggleMenu = () => {
+        setIsMenuVisible(!isMenuVisible);
+    };
+
+
+    if (!isHamburgerMenu) {
+        return (
+            <nav>
+                <div className="eighty-width nav-container">
+                    <a href="https://jessieinit.com/" id="logo">{'>'}_Jessie_in_IT</a>
+                    <ul>
+                        <li>
+                            <a className="nav-link" href="https://jessieinit.com/">Home</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#about-me">About Me</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#skills">Skills</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#experience">Experience</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#projects">Projects</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#blog">Blog</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+
+    if (isHamburgerMenu) {
+        return (
+            <Fragment>
+                <nav>
+                    <div className="eighty-width nav-container">
+                        <a href="https://jessieinit.com/" id="logo">{'>'}_Jessie_in_IT</a>
+                        <button 
+                        className="hamburger"
+                        onClick={toggleMenu}>
+                            {!isMenuVisible
+                                ? <GiHamburgerMenu
+                                    className="menuIcon"
+                                />
+                                : <GrClose
+                                    className="closeIcon"
+                                />
+                            }
+                        </button>
+                    </div>
+                </nav>
+
+                <ul className={`menu ${isMenuVisible && "showMenu"}`}>
                     <li>
-                        <a className="nav-link" href="https://jessieinit.com/">Home</a>
+                        <a className="menuItem" href="https://jessieinit.com/">Home</a>
                     </li>
                     <li>
-                        <a className="nav-link" href="#about-me">About Me</a>
+                        <a className="menuItem" href="#about-me">About Me</a>
                     </li>
                     <li>
-                        <a className="nav-link" href="#skills">Skills</a>
+                        <a className="menuItem" href="#skills">Skills</a>
                     </li>
                     <li>
-                        <a className="nav-link" href="#experience">Experience</a>
+                        <a className="menuItem" href="#experience">Experience</a>
                     </li>
                     <li>
-                        <a className="nav-link" href="#projects">Projects</a>
+                        <a className="menuItem" href="#projects">Projects</a>
                     </li>
                     <li>
-                        <a className="nav-link" href="#blog">Blog</a>
+                        <a className="menuItem" href="#blog">Blog</a>
                     </li>
                 </ul>
-            </div>
-        </nav>
-    )
+            </Fragment>
+        )
+    }
+
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import './Projects.css';
 import { Button } from '../../UI/Button';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from "swiper";
+import './Projects.css';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "./styles.css";
+
 
 export const Projects = () => {
     return (
@@ -12,87 +19,111 @@ export const Projects = () => {
                 <div className="eighty-width boxshadow slide-container">
                     <div className="slide-content">
                         <div className="card-wrapper">
+                            <Swiper
+                                effect={"coverflow"}
+                                grabCursor={true}
+                                centeredSlides={true}
+                                slidesPerView={"auto"}
+                                coverflowEffect={{
+                                  rotate: 50,
+                                  stretch: 0,
+                                  depth: 100,
+                                  modifier: 1,
+                                  slideShadows: false,
+                                }}
+                                pagination={true}
+                                modules={[EffectCoverflow, Pagination]}
+                                className="mySwiper"
+                            >
+                                <SwiperSlide>
+                                    <div className="boxshadow card">
+                                        <div className="image-content">
+                                            <span className="overlay"></span>
+                                            <div className="card-image">
+                                                <img src="../../../image/Project_Purrcolator.png"
+                                                    alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                            </div>
+                                        </div>
 
-                            <div className="boxshadow card">
-                                <div className="image-content">
-                                    <span className="overlay"></span>
-                                    <div className="card-image">
-                                        <img src="../../../image/Project_Purrcolator.png"
-                                            alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                        <div className="card-content">
+                                            <h3 className="project-name">Puurcolator</h3>
+                                            <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
+                                                reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
+                                            <Button
+                                                className="basic-button view-more-button"
+                                                href="#"
+                                            >View More</Button>
+                                        </div>
                                     </div>
-                                </div>
+                                </SwiperSlide>
 
-                                <div className="card-content">
-                                    <h3 className="project-name">Puurcolator</h3>
-                                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
-                                        reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
-                                    <Button
-                                        className="basic-button view-more-button"
-                                        href="#"
-                                    >View More</Button>
-                                </div>
-                            </div>
+                                <SwiperSlide>
+                                    <div className="card">
+                                        <div className="image-content">
+                                            <span className="overlay"></span>
+                                            <div className="card-image">
+                                                <img src="../../../image/Project_Fotomatic.png"
+                                                    alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                            </div>
+                                        </div>
+                                        <div className="card-content">
+                                            <h3 className="project-name">Fotomatic</h3>
+                                            <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
+                                                reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
 
-                            <div className="card">
-                                <div className="image-content">
-                                    <span className="overlay"></span>
-                                    <div className="card-image">
-                                        <img src="../../../image/Project_Fotomatic.png"
-                                            alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                            <Button
+                                                className="basic-button view-more-button"
+                                                href="#"
+                                            >View More</Button>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="card-content">
-                                    <h3 className="project-name">Fotomatic</h3>
-                                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
-                                        reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
+                                </SwiperSlide>
 
-                                    <Button
-                                        className="basic-button view-more-button"
-                                        href="#"
-                                    >View More</Button>
-                                </div>
-                            </div>
+                                <SwiperSlide>
+                                    <div className="card">
+                                        <div className="image-content">
+                                            <span className="overlay"></span>
+                                            <div className="card-image">
+                                                <img src="../../../image/Project_CSS_Visual_Rules.png"
+                                                    alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                            </div>
+                                        </div>
 
-                            <div className="card">
-                                <div className="image-content">
-                                    <span className="overlay"></span>
-                                    <div className="card-image">
-                                        <img src="../../../image/Project_CSS_Visual_Rules.png"
-                                            alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                        <div className="card-content">
+                                            <h3 className="project-name">CSS Visual Rules</h3>
+                                            <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
+                                                reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
+                                            <Button
+                                                className="basic-button view-more-button"
+                                                href="#"
+                                            >View More</Button>
+                                        </div>
                                     </div>
-                                </div>
+                                </SwiperSlide>
 
-                                <div className="card-content">
-                                    <h3 className="project-name">CSS Visual Rules</h3>
-                                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
-                                        reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
-                                    <Button
-                                        className="basic-button view-more-button"
-                                        href="#"
-                                    >View More</Button>
-                                </div>
-                            </div>
+                                <SwiperSlide>
+                                    <div className="card">
+                                        <div className="image-content">
+                                            <span className="overlay"></span>
+                                            <div className="card-image">
+                                                <img src="../../../image/Project_Tea_Cozy.png"
+                                                    alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                            </div>
+                                        </div>
 
-                            <div className="card">
-                                <div className="image-content">
-                                    <span className="overlay"></span>
-                                    <div className="card-image">
-                                        <img src="../../../image/Project_Tea_Cozy.png"
-                                            alt="Web development project for an imaginary cat cafe called Purrcolator" className="card-img" />
+                                        <div className="card-content">
+                                            <h3 className="project-name">Tea Cozy</h3>
+                                            <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
+                                                reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
+                                            <Button
+                                                className="basic-button view-more-button"
+                                                href="#"
+                                            >View More</Button>
+
+                                        </div>
                                     </div>
-                                </div>
-
-                                <div className="card-content">
-                                    <h3 className="project-name">Tea Cozy</h3>
-                                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem natus
-                                        reiciendis ducimus, fugit nobis consequatur laborum mollitia quod.</p>
-                                    <Button
-                                        className="basic-button view-more-button"
-                                        href="#"
-                                    >View More</Button>
-
-                                </div>
-                            </div>
+                                </SwiperSlide>
+                            </Swiper>
                         </div>
                     </div>
                 </div>
